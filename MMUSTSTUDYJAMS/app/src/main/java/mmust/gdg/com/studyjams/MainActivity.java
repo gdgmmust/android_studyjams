5018d207cab75a67d2dba34ac02a1e6e05444114
marviktintor <vmwenda.vm@gmail.com>
2015-04-08 09:05:35 +0300

    Android Study Jams
    The Sample App we created in GDG MMUST During the STudy Jams Event

---
+++ MMUSTSTUDYJAMS/app/src/main/java/mmust/gdg/com/studyjams/MainActivity.java
@@ -0,0 +1,15 @@
+package mmust.gdg.com.studyjams;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by victor_mwenda on 07-Feb-15.
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
